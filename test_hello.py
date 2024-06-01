7402133bad6b234bd518cf107c084760c47b9585
FedericoAlmiron02 <fede.almiron02@gmail.com>
2024-06-01 14:28:04 -0300

    Update test_hello.py

--- test_hello.py
+++ test_hello.py
@@ -8,8 +8,3 @@ def test_suma():
     assert suma(1.5, 2.5) == 4.0
     assert suma(-1.5, -2.5) == -4.0
 
-def test_suma_float():
-    assert suma(1.1, 2.2) == 3.3
-    assert suma(-1.1, -2.2) == -3.3
-    assert suma(1.1, -2.2) == -1.1
-    assert suma(-1.1, 2.2) == 1.1
