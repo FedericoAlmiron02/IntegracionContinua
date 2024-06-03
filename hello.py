1cad12f05b148d05f5172931c10a0b1c2720ab45
Federico Almiron <fede.almiron02@gmail.com>
2024-06-02 22:31:55 -0300

    modifique el yml (requirementstxt)

--- hello.py
+++ hello.py
@@ -1,7 +1,6 @@
 def suma(a, b):
     return a + b
 
-
 if __name__ == "__main__":
     resultado = suma(5, 4)
     print("La suma  es:", resultado)
--- test_hello.py
+++ test_hello.py
@@ -7,4 +7,6 @@ def test_suma():
     assert suma(0, 0) == 0
     assert suma(1.5, 2.5) == 4.0
     assert suma(-1.5, -2.5) == -4.0
+    assert suma(1.23456789, 9.87654321) == 11.1111111
+
 
